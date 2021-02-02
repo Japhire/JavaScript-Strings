2f071dab6b5f04cf38c845d511a651f0b87bdbce
Edward Crawford <erac915@icloud.com>
2021-02-02 15:52:28 -0500

    add

---
+++ js/basics-2.js
@@ -0,0 +1,15 @@
+
+const userName = prompt("enter some text");
+
+const greeting =('Hello There ')
+
+document.getElementById('para').textContent = greeting + userName;
+
+// console.log ("userText has " +  userText.length + " " + "characters in it")
+// console.log("Let's do this!")
+
+// console.log(userText.indexOf ("i"))
+
+// console.log(userText.slice(5));
+
+// console.log(userText.charAt(1));
