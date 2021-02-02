00457be81201e50b513875c0d1598c913cf08a4b
Edward Crawford <erac915@icloud.com>
2021-02-02 15:23:32 -0500

    step 6

--- js/basics-1.js
+++ js/basics-1.js
@@ -4,4 +4,8 @@ const userText = prompt("enter some text");
 console.log ("userText has " +  userText.length + " " + "characters in it")
 // console.log("Let's do this!")
 
-console.log(userText.indexOf ("i"))
+// console.log(userText.indexOf ("i"))
+
+// console.log(userText.slice(5));
+
+console.log(userText.charAt(1));
